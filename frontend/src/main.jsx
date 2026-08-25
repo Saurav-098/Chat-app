080464617f7f0c08ccef34d4621e5ed80ea57b80
Saurav-098 <sauravkumar765423@gmail.com>
2026-08-25 16:26:39 +0530

    Update server URL in main .jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider} from 'react-redux';
 import { store } from './store/store.jsx';
-export const serverUrl = "http://localhost:3000"
+export const serverUrl = "https://chat-app-backend-avq9.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
